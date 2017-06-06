ae40acab0127ff219957f3c4c2f5b41e1e083ca2
drutkowski <drutkowski@HBS-DRUTKOWSKI>
2017-06-06 15:27:48 -0500

    Finished Styling for MyEvents, EventsDetails, and Survey

--- HBS.ITAG/HBS.ITAG/Resources/Resource.Designer.cs
+++ HBS.ITAG/HBS.ITAG/Resources/Resource.Designer.cs
@@ -79,25 +79,28 @@ namespace HBS.ITAG
 			public const int calendar = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int house = 2130837508;
+			public const int custom_scroll_style = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Icon = 2130837509;
+			public const int house = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int inProgress = 2130837510;
+			public const int Icon = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int ITAG = 2130837511;
+			public const int inProgress = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int profile = 2130837512;
+			public const int ITAG = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int Upcoming = 2130837513;
+			public const int profile = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int wrench = 2130837514;
+			public const int Upcoming = 2130837514;
+			
+			// aapt resource value: 0x7f02000b
+			public const int wrench = 2130837515;
 			
 			static Drawable()
 			{
