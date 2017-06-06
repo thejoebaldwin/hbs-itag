fdf2d375fcd1e143a835651a68d80b13c45ec4ef
kfrancois <kfrancois@HBS-KFRANCOIS>
2017-06-06 11:01:35 -0500

    Added App features cs page and axml page. Also added pictures for app features page

---
+++ HBS.ITAG/HBS.ITAG/AppFeatures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HBS.ITAG
+{
+    [Activity(Label = "AppFeatures")]
+    public class AppFeatures : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.AppFeatures);
+            Button BluetoothMsg = FindViewById<Button>(Resource.Id.BluetoothMsg);
+
+            ImageView WifiTowerPicture = FindViewById<ImageView>(Resource.Id.bluetoothimage);
+            TextView Title = FindViewById<TextView>(Resource.Id.Title);
+            TextView Header1 = FindViewById<TextView>(Resource.Id.Header1);
+            TextView Info1 = FindViewById<TextView>(Resource.Id.Info1);
+            TextView Header2 = FindViewById<TextView>(Resource.Id.Header2);
+            ImageView Blueprintimage = FindViewById<ImageView>(Resource.Id.blueprintexample);
+            TextView Header3 = FindViewById<TextView>(Resource.Id.Header3);
+
+        }
+    }
+}
--- HBS.ITAG/HBS.ITAG/Resources/Resource.Designer.cs
+++ HBS.ITAG/HBS.ITAG/Resources/Resource.Designer.cs
@@ -67,7 +67,13 @@ namespace HBS.ITAG
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int blueprintexample = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int bluetooth = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
 			
 			static Drawable()
 			{
@@ -82,104 +88,146 @@ namespace HBS.ITAG
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060011
-			public const int DSbutton1 = 2131099665;
-			
-			// aapt resource value: 0x7f060010
-			public const int DScheckBox1 = 2131099664;
-			
-			// aapt resource value: 0x7f06000b
-			public const int DSedittext = 2131099659;
-			
-			// aapt resource value: 0x7f06000e
-			public const int DSedittext2 = 2131099662;
-			
-			// aapt resource value: 0x7f060001
-			public const int DSrelativeLayout1 = 2131099649;
-			
-			// aapt resource value: 0x7f060004
-			public const int DSrelativeLayout2 = 2131099652;
-			
-			// aapt resource value: 0x7f060007
-			public const int DSrelativeLayout3 = 2131099655;
-			
-			// aapt resource value: 0x7f06000a
-			public const int DSrelativeLayout4 = 2131099658;
-			
-			// aapt resource value: 0x7f06000d
-			public const int DSrelativeLayout5 = 2131099661;
-			
-			// aapt resource value: 0x7f060003
-			public const int DSspinner1 = 2131099651;
-			
-			// aapt resource value: 0x7f060006
-			public const int DSspinner2 = 2131099654;
-			
-			// aapt resource value: 0x7f060009
-			public const int DSspinner3 = 2131099657;
-			
 			// aapt resource value: 0x7f060002
-			public const int DStextView2 = 2131099650;
-			
-			// aapt resource value: 0x7f060005
-			public const int DStextView3 = 2131099653;
-			
-			// aapt resource value: 0x7f060008
-			public const int DStextView4 = 2131099656;
-			
-			// aapt resource value: 0x7f06000c
-			public const int DStextView5 = 2131099660;
-			
-			// aapt resource value: 0x7f06000f
-			public const int DStextView6 = 2131099663;
-			
-			// aapt resource value: 0x7f060000
-			public const int DStextView7 = 2131099648;
-			
-			// aapt resource value: 0x7f060014
-			public const int EDimageButton1 = 2131099668;
-			
-			// aapt resource value: 0x7f060012
-			public const int EDrelativeLayout1 = 2131099666;
-			
-			// aapt resource value: 0x7f060015
-			public const int EDrelativeLayout2 = 2131099669;
-			
-			// aapt resource value: 0x7f060018
-			public const int EDrelativeLayout3 = 2131099672;
-			
-			// aapt resource value: 0x7f06001b
-			public const int EDrelativeLayout4 = 2131099675;
+			public const int BluetoothMsg = 2131099650;
 			
 			// aapt resource value: 0x7f06001e
-			public const int EDrelativeLayout5 = 2131099678;
-			
-			// aapt resource value: 0x7f060013
-			public const int EDtextView1 = 2131099667;
-			
-			// aapt resource value: 0x7f060016
-			public const int EDtextView2 = 2131099670;
-			
-			// aapt resource value: 0x7f060017
-			public const int EDtextView3 = 2131099671;
-			
-			// aapt resource value: 0x7f060019
-			public const int EDtextView4 = 2131099673;
-			
-			// aapt resource value: 0x7f06001a
-			public const int EDtextView5 = 2131099674;
-			
-			// aapt resource value: 0x7f06001c
-			public const int EDtextView6 = 2131099676;
+			public const int DSbutton1 = 2131099678;
 			
 			// aapt resource value: 0x7f06001d
-			public const int EDtextView7 = 2131099677;
+			public const int DScheckBox1 = 2131099677;
+			
+			// aapt resource value: 0x7f060018
+			public const int DSedittext = 2131099672;
+			
+			// aapt resource value: 0x7f06001b
+			public const int DSedittext2 = 2131099675;
+			
+			// aapt resource value: 0x7f06000e
+			public const int DSrelativeLayout1 = 2131099662;
+			
+			// aapt resource value: 0x7f060011
+			public const int DSrelativeLayout2 = 2131099665;
+			
+			// aapt resource value: 0x7f060014
+			public const int DSrelativeLayout3 = 2131099668;
+			
+			// aapt resource value: 0x7f060017
+			public const int DSrelativeLayout4 = 2131099671;
+			
+			// aapt resource value: 0x7f06001a
+			public const int DSrelativeLayout5 = 2131099674;
+			
+			// aapt resource value: 0x7f060010
+			public const int DSspinner1 = 2131099664;
+			
+			// aapt resource value: 0x7f060013
+			public const int DSspinner2 = 2131099667;
+			
+			// aapt resource value: 0x7f060016
+			public const int DSspinner3 = 2131099670;
+			
+			// aapt resource value: 0x7f06000f
+			public const int DStextView2 = 2131099663;
+			
+			// aapt resource value: 0x7f060012
+			public const int DStextView3 = 2131099666;
+			
+			// aapt resource value: 0x7f060015
+			public const int DStextView4 = 2131099669;
+			
+			// aapt resource value: 0x7f060019
+			public const int DStextView5 = 2131099673;
+			
+			// aapt resource value: 0x7f06001c
+			public const int DStextView6 = 2131099676;
+			
+			// aapt resource value: 0x7f06000d
+			public const int DStextView7 = 2131099661;
+			
+			// aapt resource value: 0x7f060021
+			public const int EDimageButton1 = 2131099681;
 			
 			// aapt resource value: 0x7f06001f
-			public const int EDtextView8 = 2131099679;
+			public const int EDrelativeLayout1 = 2131099679;
+			
+			// aapt resource value: 0x7f060022
+			public const int EDrelativeLayout2 = 2131099682;
+			
+			// aapt resource value: 0x7f060025
+			public const int EDrelativeLayout3 = 2131099685;
+			
+			// aapt resource value: 0x7f060028
+			public const int EDrelativeLayout4 = 2131099688;
+			
+			// aapt resource value: 0x7f06002b
+			public const int EDrelativeLayout5 = 2131099691;
 			
 			// aapt resource value: 0x7f060020
-			public const int EDtextView9 = 2131099680;
+			public const int EDtextView1 = 2131099680;
+			
+			// aapt resource value: 0x7f060023
+			public const int EDtextView2 = 2131099683;
+			
+			// aapt resource value: 0x7f060024
+			public const int EDtextView3 = 2131099684;
+			
+			// aapt resource value: 0x7f060026
+			public const int EDtextView4 = 2131099686;
+			
+			// aapt resource value: 0x7f060027
+			public const int EDtextView5 = 2131099687;
+			
+			// aapt resource value: 0x7f060029
+			public const int EDtextView6 = 2131099689;
+			
+			// aapt resource value: 0x7f06002a
+			public const int EDtextView7 = 2131099690;
+			
+			// aapt resource value: 0x7f06002c
+			public const int EDtextView8 = 2131099692;
+			
+			// aapt resource value: 0x7f06002d
+			public const int EDtextView9 = 2131099693;
+			
+			// aapt resource value: 0x7f060006
+			public const int Header1 = 2131099654;
+			
+			// aapt resource value: 0x7f060009
+			public const int Header2 = 2131099657;
+			
+			// aapt resource value: 0x7f06000b
+			public const int Header3 = 2131099659;
+			
+			// aapt resource value: 0x7f060008
+			public const int Info1 = 2131099656;
+			
+			// aapt resource value: 0x7f06000c
+			public const int Info2 = 2131099660;
+			
+			// aapt resource value: 0x7f060004
+			public const int Title = 2131099652;
+			
+			// aapt resource value: 0x7f06000a
+			public const int blueprintexample = 2131099658;
+			
+			// aapt resource value: 0x7f060001
+			public const int bluetoothimage = 2131099649;
+			
+			// aapt resource value: 0x7f060005
+			public const int gridLayout1 = 2131099653;
+			
+			// aapt resource value: 0x7f060003
+			public const int gridLayout3 = 2131099651;
+			
+			// aapt resource value: 0x7f060007
+			public const int gridLayout5 = 2131099655;
+			
+			// aapt resource value: 0x7f060000
+			public const int gridLayout7 = 2131099648;
+			
+			// aapt resource value: 0x7f06002e
+			public const int textView1 = 2131099694;
 			
 			static Id()
 			{
@@ -195,16 +243,19 @@ namespace HBS.ITAG
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int DemographicSurvey = 2130903040;
+			public const int AppFeatures = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int EventDetails = 2130903041;
+			public const int DemographicSurvey = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int Main = 2130903042;
+			public const int EventDetails = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int Schedule = 2130903043;
+			public const int Main = 2130903043;
+			
+			// aapt resource value: 0x7f030004
+			public const int Schedule = 2130903044;
 			
 			static Layout()
 			{
@@ -224,6 +275,12 @@ namespace HBS.ITAG
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040003
+			public const int Info1 = 2130968579;
+			
+			// aapt resource value: 0x7f040004
+			public const int Info2 = 2130968580;
 			
 			// aapt resource value: 0x7f040002
 			public const int surveyDescription = 2130968578;
