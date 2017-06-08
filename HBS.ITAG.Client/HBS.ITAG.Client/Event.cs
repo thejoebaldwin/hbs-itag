75265ab2e0a49df4ae808e4a6fe063ce0c7d0e01
Joe Baldwin <contact@humboldttechgroup.com>
2017-06-08 14:50:21 -0500

    Parse local events json text file into event objects

---
+++ HBS.ITAG.Client/HBS.ITAG.Client/Event.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HBS.ITAG.Client
+{
+
+    public class Event
+    {
+
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Presenter { get; set; }
+        public string Summary { get; set; }
+        public int Track { get; set; }
+        public int EventWebId { get; set; }
+
+
+        public Event(string name, int id, DateTime startTime, DateTime endTime, string presenter, string summary, int eventWebId)
+        {
+            Name = name;
+            Id = id;
+       }
+
+        public static Event FromJson(string json)
+        {
+            System.Collections.Generic.Dictionary<string, string> data = HBS.ITAG.Client.Utilities.ParseJson(json);
+
+            //dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+            string name = data["name"];
+            int id = System.Convert.ToInt32(data["event_id"]);
+            DateTime startTime = DateTime.Parse(data["start_time"]);
+            DateTime endTime = DateTime.Parse(data["end_time"]);
+            string presenter = data["presenter"];
+            string summary = data["summary"];
+            //int eventWebId = data["event_web_id"];
+
+            return new Event(name, id, startTime, endTime, presenter, summary, -1);
+        }
+    }
+}
--- HBS.ITAG.Client/HBS.ITAG.Client/Program.cs
+++ HBS.ITAG.Client/HBS.ITAG.Client/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Collections;
 
 namespace HBS.ITAG.Client
 {
@@ -9,6 +11,16 @@ namespace HBS.ITAG.Client
     {
         static void Main(string[] args)
         {
+            string json = System.IO.File.ReadAllText("../../events.txt");
+            System.Collections.Generic.Dictionary<string, string> data = HBS.ITAG.Client.Utilities.ParseJson(json);
+            string eventJson = data["events"];
+            string[] events = HBS.ITAG.Client.Utilities.ParseJsonArray(eventJson);
+            ArrayList arrEvents = new ArrayList();
+            foreach (string s in events)
+            {
+                Event tempEvent = Event.FromJson(s);
+                arrEvents.Add(tempEvent);
+            }
         }
     }
 }
