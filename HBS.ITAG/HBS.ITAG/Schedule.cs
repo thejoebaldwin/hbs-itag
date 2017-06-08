f1afc1ce57b6ea8874fa26d7d9f7fce54263fca0
Kyle Van Hammond <kvanhammond@hbs.net>
2017-06-08 08:19:21 -0500

    Ripple on Schedule Tabs!

--- HBS.ITAG/HBS.ITAG/Schedule.cs
+++ HBS.ITAG/HBS.ITAG/Schedule.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Forms;
 
 namespace HBS.ITAG
 {
@@ -18,46 +19,69 @@ namespace HBS.ITAG
         {
             RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.Schedule);
+            SetContentView(Resource.Layout.Carousel);
 
             //ImageView leftArrow = FindViewById<ImageView>(Resource.Id.left_arrow);
             //ImageView rightArrow = FindViewById<ImageView>(Resource.Id.right_arrow);
 
-            Button day1btn = FindViewById<Button>(Resource.Id.day_one);
-            Button day2btn = FindViewById<Button>(Resource.Id.day_two);
-            Button day3btn = FindViewById<Button>(Resource.Id.day_three);
-            Button day4btn = FindViewById<Button>(Resource.Id.day_four);
+            Android.Widget.Button day1btn = FindViewById<Android.Widget.Button>(Resource.Id.day_one_btn);
+            Android.Widget.Button day2btn = FindViewById<Android.Widget.Button>(Resource.Id.day_two_btn);
+            Android.Widget.Button day3btn = FindViewById<Android.Widget.Button>(Resource.Id.day_three_btn);
+            Android.Widget.Button day4btn = FindViewById<Android.Widget.Button>(Resource.Id.day_four_btn);
+
+            FrameLayout day1 = FindViewById<FrameLayout>(Resource.Id.day_one);
+            FrameLayout day2 = FindViewById<FrameLayout>(Resource.Id.day_two);
+            FrameLayout day3 = FindViewById<FrameLayout>(Resource.Id.day_three);
+            FrameLayout day4 = FindViewById<FrameLayout>(Resource.Id.day_four);
+
+            CarouselView day1tracks = FindViewById<CarouselView>(Resource.Id.day1tracks);
 
             day1btn.Click += delegate 
             {
-                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_ripple));
-                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
+                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_day));
+                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day1.Visibility = ViewStates.Visible;
+                day2.Visibility = ViewStates.Gone;
+                day3.Visibility = ViewStates.Gone;
+                day4.Visibility = ViewStates.Gone;
             };
 
             day2btn.Click += delegate
             {
-                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_ripple));
-                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
+                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_day));
+                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day1.Visibility = ViewStates.Gone;
+                day2.Visibility = ViewStates.Visible;
+                day3.Visibility = ViewStates.Gone;
+                day4.Visibility = ViewStates.Gone;
             };
 
             day3btn.Click += delegate
             {
-                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_ripple));
-                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
+                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_day));
+                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day1.Visibility = ViewStates.Gone;
+                day2.Visibility = ViewStates.Gone;
+                day3.Visibility = ViewStates.Visible;
+                day4.Visibility = ViewStates.Gone;
             };
 
             day4btn.Click += delegate
             {
-                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_ripple));
-                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_ripple));
+                day1btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day2btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day3btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.unselected_day));
+                day4btn.SetBackgroundDrawable(Resources.GetDrawable(Resource.Drawable.selected_day));
+                day1.Visibility = ViewStates.Gone;
+                day2.Visibility = ViewStates.Gone;
+                day3.Visibility = ViewStates.Gone;
+                day4.Visibility = ViewStates.Visible;
             };
 
             //TextView TrackTitle = FindViewById<TextView>(Resource.Id.track_title);
