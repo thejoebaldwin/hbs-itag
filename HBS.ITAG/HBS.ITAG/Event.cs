a15226b1d76919732b99983195dcdda03d41314c
drutkowski <drutkowski@HBS-DRUTKOWSKI>
2017-06-08 13:23:50 -0500

    Added Classes for JSON

---
+++ HBS.ITAG/HBS.ITAG/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HBS.ITAG
+{
+    class Event
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Presenter { get; set; }
+        public string Summary { get; set; }
+        public int Track { get; set; }
+        public int EventWebId { get; set; }
+    }
+}
---
+++ HBS.ITAG/HBS.ITAG/Session.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace HBS.ITAG
+{
+    class Session
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public int EventId { get; set; }
+        public int UserId { get; set; }
+    }
+}
