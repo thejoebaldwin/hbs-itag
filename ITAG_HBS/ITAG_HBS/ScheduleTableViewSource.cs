ae112e4ac8c4146bbc8e02b9037a4577297b3b52
kvanhammond <kvanhammond@users.noreply.github.com>
2017-06-13 11:58:20 -0500

    Made Schedule Page Additions

--- ITAG_HBS/ITAG_HBS/ScheduleTableViewSource.cs
+++ ITAG_HBS/ITAG_HBS/ScheduleTableViewSource.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using UIKit;
 using Foundation;
 
@@ -7,32 +7,8 @@ namespace ITAG_HBS
 	public class ScheduleTableViewSource : UITableViewSource
 	{
 
-		string[] TableItems;
-		string CellIdentifier = "TableCell";
-
-		public ScheduleTableViewSource(string[] items)
-		{
-			TableItems = items;
-		}
-
-		public override nint RowsInSection(UITableView tableview, nint section)
-		{
-			return TableItems.Length;
-		}
-
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
-			UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
-			string item = TableItems[indexPath.Row];
-
-			//---- if there are no cells to reuse, create a new one
-			if (cell == null)
-			{ cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); }
-
-			cell.TextLabel.Text = item;
-
-
-			return cell;
-		}
-	}
+		string[] TableItems; 	    string CellIdentifier = "TableCell";  	    public ScheduleTableViewSource(string[] items)
+	    { 	        TableItems = items; 	    }  	    public override nint RowsInSection(UITableView tableview, nint section)
+	    { 	        return TableItems.Length; 	    }  	    public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath) 	    { 	        UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier); 	        string item = TableItems[indexPath.Row];  	        //---- if there are no cells to reuse, create a new one 	        if (cell == null) 	        { 	            cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier); 	        } 	        if (item == "Event") 	        { 	            cell.BackgroundColor = ITAG.HBS.UIColorExtension.FromHex(0x0E1D52); 	            cell.TextLabel.TextColor = UIColor.White; 	        } 	        else if(item == "Lunch") 	        { 	            cell.BackgroundColor = ITAG.HBS.UIColorExtension.FromHex(0x99A1AC); 	            cell.TextLabel.TextColor = UIColor.Black; 	        } 	        else
+	        { 	            cell.BackgroundColor = UIColor.White; 	            cell.TextLabel.TextColor = UIColor.Black; 	        }  	        cell.TextLabel.Text = item;  	       return cell; 	    }     }
 }
---
+++ ITAG_HBS/ITAG_HBS/UIColorExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UIKit;
+
+namespace ITAG.HBS
+{
+    public class UIColorExtension
+    {
+		public static UIColor FromHex(int hexValue)
+		{
+			return UIColor.FromRGB(
+				(((hexValue & 0xFF0000) >> 16) / 255.0f),
+				(((hexValue & 0xFF00) >> 8) / 255.0f),
+				((hexValue & 0xFF) / 255.0f)
+			);
+		}
+    }
+}
