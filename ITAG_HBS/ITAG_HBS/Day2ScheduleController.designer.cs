67512ec3f6d36f2427f91b08fa7fa34e44cb1aeb
kvanhammond <kvanhammond@users.noreply.github.com>
2017-06-14 13:25:34 -0500

    Made Schedule Page Refresh Data depending on Track and Added Swiping between tracks on all 4 days

--- ITAG_HBS/ITAG_HBS/Day2ScheduleController.designer.cs
+++ ITAG_HBS/ITAG_HBS/Day2ScheduleController.designer.cs
@@ -16,13 +16,40 @@ namespace ITAG.HBS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView D2LeftArrow { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView D2RightArrow { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UITextView Day2TrackName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView DayTwo { get; set; }
 
         void ReleaseDesignerOutlets ()
         {
+            if (D2LeftArrow != null) {
+                D2LeftArrow.Dispose ();
+                D2LeftArrow = null;
+            }
+
+            if (D2RightArrow != null) {
+                D2RightArrow.Dispose ();
+                D2RightArrow = null;
+            }
+
             if (Day2TrackName != null) {
                 Day2TrackName.Dispose ();
                 Day2TrackName = null;
+            }
+
+            if (DayTwo != null) {
+                DayTwo.Dispose ();
+                DayTwo = null;
             }
         }
     }
