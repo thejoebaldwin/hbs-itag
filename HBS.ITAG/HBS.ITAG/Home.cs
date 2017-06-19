6752d0d0a5c557f41a85f32d3377468676850221
Joe Baldwin <contact@humboldttechgroup.com>
2017-06-19 10:58:29 -0500

    Adding estimate code

--- HBS.ITAG/HBS.ITAG/Home.cs
+++ HBS.ITAG/HBS.ITAG/Home.cs
@@ -9,13 +9,39 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using HBS.ITAG.Model;
+using EstimoteSdk;
+
 
 namespace HBS.ITAG
 {
     [Activity(Label = "Home", ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
-    public class Home : Activity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
+    public class Home : Activity, BeaconManager.IServiceReadyCallback
+	{
+
+		BeaconManager beaconManager;
+		const string PROXIMITY_UUID = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
+
+        public bool isEmulator()
+        {
+			string fing = Build.Fingerprint;
+			bool isEmulator = false;
+			if (fing != null)
+			{
+				isEmulator = fing.Contains("vbox") || fing.Contains("generic");
+			}
+            return isEmulator;
+        }
+
+		public void OnServiceReady()
+		{
+            if (!isEmulator())
+            {
+                beaconManager = new BeaconManager(this);
+            }
+			Store.Instance.GetTracks(LoadTracksComplete);
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
         {
             RequestWindowFeature(WindowFeatures.NoTitle);
             base.OnCreate(savedInstanceState);
@@ -87,7 +113,105 @@ namespace HBS.ITAG
             //Store.Instance.LoadEventsFromFile();
             //Store.Instance.LoadTracksFromFile();
 
-
+        
         }
-    }
+
+		private void LoadTracksComplete(string message)
+		{
+			Store.Instance.GetEvents(LoadEventsComplete);
+		}
+
+		private void LoadEventsComplete(string message)
+		{
+			Store.Instance.GetLocations(LoadLocationsComplete);
+		}
+
+		private void LoadLocationsComplete(string message)
+		{
+            if (!isEmulator())
+            {
+			InitializeBeacons();
+                }
+		}
+
+		private void InitializeBeacons()
+		{
+			//run on main thread
+		
+			   //loop through all location entries
+			   for (int i = 0; i < Store.Instance.Locations.Count; i++)
+				{
+					Location tempLocation = Store.Instance.Locations[i];
+                   //create new region
+                    Region beaconRegion = new Region(tempLocation.Nickname, PROXIMITY_UUID,System.Convert.ToInt32(tempLocation.Major), System.Convert.ToInt32(tempLocation.Minor));
+                    beaconManager.StartMonitoring(beaconRegion);
+				}
+
+			//on region exit
+		   beaconManager.ExitedRegion += (sender, e) =>
+	       {
+		    if (Store.Instance.Notify)
+		    {
+          
+			  Event tempEvent = Store.Instance.ProximityEvent(e.P0.Major.ToString(), e.P0.Minor.ToString());
+			  if (tempEvent != null)
+			  {
+				  OnRegionExit(tempEvent);
+			  }
+		    }
+	      };
+
+
+			beaconManager.EnteredRegion += (sender, e) =>
+		 {
+			 if (Store.Instance.Notify)
+			 {
+				 Event tempEvent = Store.Instance.ProximityEvent(e.Region.Major.ToString(), e.Region.Minor.ToString());
+				 if (tempEvent != null)
+				 {
+					     OnRegionEnter(tempEvent);
+				   }
+			 }
+		 };
+
+
+		}
+
+		public void OnRegionExit(Event tempEvent)
+		{
+			int minutesSinceLastNotification = (tempEvent.LastExitNotified - DateTime.Now).Minutes;
+			minutesSinceLastNotification = Math.Abs(minutesSinceLastNotification);
+			if (minutesSinceLastNotification > 5)
+			{
+				Store.Instance.AddSession(tempEvent.Id, false, OnSessionAddComplete);
+				tempEvent.LastExitNotified = DateTime.Now;
+			}
+		}
+
+
+		public void OnRegionEnter(Event tempEvent)
+		{
+			int minutesSinceLastNotification = (tempEvent.LastEntryNotified - DateTime.Now).Minutes;
+			minutesSinceLastNotification = Math.Abs(minutesSinceLastNotification);
+			//don't notify twice in a row and don't repeat the same notification more than once in 10 minutes
+			if (Store.Instance.SelectedEvent != tempEvent && minutesSinceLastNotification > 5)
+			{
+				//TODO: If app open, ask user if they want to see the information
+                //      if app closed, add notification that event is in range
+				tempEvent.LastEntryNotified = DateTime.Now;
+				Store.Instance.AddSession(tempEvent.Id, true, OnSessionAddComplete);
+
+			}
+
+		}
+
+        public void OnSessionAddComplete(string message)
+        {
+            
+        }
+
+
+
+
+	}
 }
